5201852694f4cae1c1ce107493787154889316cd
TheCodeTherapy <me@mgz.me>
2023-09-20 13:12:14 +0100

    updates grace period for unresponsive connections on user networking

--- packages/3d-web-user-networking/src/user-networking-settings.ts
+++ packages/3d-web-user-networking/src/user-networking-settings.ts
@@ -1,3 +1,3 @@
-export const pingPongRate: number = 1500;
-export const heartBeatRate: number = 30000;
+export const pingPongRate: number = 1000;
+export const heartBeatRate: number = 10000;
 export const packetsUpdateRate: number = (1 / 30) * 1000;
