5079fdc0576feae451ae5b4937aff51015b32ae7
TheCodeTherapy <me@mgz.me>
2025-02-27 18:51:28 +0000

    updates default SSAO values

--- packages/3d-web-client-core/src/tweakpane/blades/ssaoFolder.ts
+++ packages/3d-web-client-core/src/tweakpane/blades/ssaoFolder.ts
@@ -41,7 +41,7 @@ const ppssaoOptions = {
 export const n8ssaoValues = {
   enabled: true,
   halfRes: true,
-  aoRadius: 5,
+  aoRadius: 8,
   distanceFalloff: 3.0,
   intensity: 2.1,
   color: { r: 0, g: 0, b: 0 },
@@ -52,7 +52,7 @@ export const n8ssaoValues = {
 };
 
 export const n8ssaoOptions = {
-  radius: { min: 0.1, max: 6, step: 0.1 },
+  radius: { min: 0.1, max: 8, step: 0.1 },
   distanceFalloff: { min: 1, max: 6, step: 0.1 },
   intensity: { min: 0.1, max: 5, step: 0.1 },
   aoSamples: [2, 4, 8, 16, 32, 64],
